ff9f2464aaca5850d66c1c1dd92aea18d94389ac
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-11-17 17:56:56 +0000

    Add Tailwind
    based on instructions at:
    https://tailwindcss.com/docs/installation/using-vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
@@ -13,7 +14,8 @@ export default defineConfig({
           dest: ''
         }
       ]
-    })
+    }),
+    tailwindcss(),
   ],
   build: {
     outDir: 'dist',
