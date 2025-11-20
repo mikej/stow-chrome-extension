f558e05538129c164ee178e0b21b3f9e65337a9c
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-11-20 13:09:08 +0000

    add basic setup form component
    add basic setup form component to prompt for API key

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div className="App">
       Extension will go here
     </div>
+  );
+}
+
+interface SetupFormProps {
+  onSave: (apiKey: string) => void;
+}
+
+function SetupForm({ onSave }: SetupFormProps) {
+  const [apiKey, setApiKey] = useState("");
+
+  return (
+    <form>
+      <div>Enter your API key to get started.</div>
+      <div>
+        <input
+          type="text"
+          value={apiKey}
+          onChange={(e) => setApiKey(e.target.value)}
+        />
+      </div>
+      <button onClick={() => onSave(apiKey)}>Continue</button>
+    </form>
   );
 }
 
